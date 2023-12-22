82a43baf7f868d274c09f84b72c75144f8007197
EgorChet <egor.chetverikov.1997@gmail.com>
2023-12-22 20:21:37 +0200

    Update - 2023-12-22 20:21:36

---
+++ Week5/Day4/JS-Basics/index.js
@@ -0,0 +1,17 @@
+
+
+let selectedColors = ['red', 'blue'];
+selectedColors[4] = 'green';
+selectedColors[2] = 'yellow';
+selectedColors[3] = 1.5;
+console.log(selectedColors);
+
+let firstName = 'Mosh';
+console.log(firstName);
+let age = 30.2
+console.log(age)
+
+console.log(typeof selectedColors)
+console.log(typeof firstName)
+console.log(typeof age)
+console.log(selectedColors.length)
