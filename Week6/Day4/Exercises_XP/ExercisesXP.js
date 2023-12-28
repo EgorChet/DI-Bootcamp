eb64a15f92bf48306acaac6730ecdb9e467c64ff
EgorChet <egor.chetverikov.1997@gmail.com>
2023-12-28 19:08:18 +0200

    Update - 2023-12-28 19:08:18

--- Week6/Day4/Exercises_XP/ExercisesXP.js
+++ Week6/Day4/Exercises_XP/ExercisesXP.js
@@ -1,4 +1,6 @@
 // Exercises XP
+console.log("Exercises_XP_Day4")
+console.log('--------');
 // Last Updated: September 22nd, 2023
 
 // What You Will Learn:
@@ -9,6 +11,7 @@
 // 🌟 Exercise 1 : Colors
 // Instructions
 // Using this array :
+console.log('Exercise 1 : Colors')
 const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
 
 // Write a JavaScript program that displays the colors in the following order : “1# choice is Blue.” “2# choice is Green.” “3# choice is Red.” ect…
@@ -22,76 +25,129 @@ colors.forEach((color, index) => {
 // Checking if "Violet" is in the array
 const containsViolet = colors.some(color => color === "Violet");
 console.log(`The array contains Violet? => ${containsViolet}`);
-
-
+console.log('--------');
 
 // 🌟 Exercise 2 : Colors #2
 // Instructions
 // Using these arrays :
-
-// const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
-// const ordinal = ["th","st","nd","rd"];
+console.log('Exercise 2 : Colors #2')
+const colors2 = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+const ordinal = ["th", "st", "nd", "rd"];
 // Write a JavaScript program that displays the colors in the following order : “1st choice is Blue .” “2nd choice is Green.” “3rd choice is Red.” ect…
-// Hint : Use the array methods taught in class and ternary operator.
 
+colors2.forEach((color, index) => {
+    // Determine the correct ordinal suffix
+    let suffix = ordinal[0]; // Default to 'th'
+    if (index === 0) {
+        suffix = ordinal[1]; // 1st
+    } else if (index === 1) {
+        suffix = ordinal[2]; // 2nd
+    } else if (index === 2) {
+        suffix = ordinal[3]; // 3rd
+    } else if (index > 2 && index < 20) {
+        suffix = ordinal[0]; // 4th to 20th
+    } else {
+        // Beyond 20, the pattern repeats every 10 (21st, 22nd, 23rd, etc.)
+        let lastDigit = (index + 1) % 10;
+        suffix = lastDigit === 1 ? ordinal[1] :
+                 lastDigit === 2 ? ordinal[2] :
+                 lastDigit === 3 ? ordinal[3] : ordinal[0];
+    }
+
+    console.log(`${index + 1}${suffix} choice is ${color}.`);
+});
+console.log('--------');
 
 // Exercise 3 : Analyzing
 // Instructions
 // Analyze these pieces of code before executing them. What will be the outputs ?
 // ------1------
-// const fruits = ["apple", "orange"];
-// const vegetables = ["carrot", "potato"];
+console.log('Exercise 3 : Analyzing')
+const fruits = ["apple", "orange"];
+const vegetables = ["carrot", "potato"];
 
-// const result = ['bread', ...vegetables, 'chicken', ...fruits];
-// console.log(result);
+const result = ['bread', ...vegetables, 'chicken', ...fruits];
+console.log(result);
+// ------1------ My prediction : ['bread', "carrot", "potato", 'chicken', "apple", "orange"]
 
 // ------2------
-// const country = "USA";
-// console.log([...country]);
-
+const country = "USA";
+console.log([...country]);
+// ------2------ My prediction : ["U", "S", "A"]
 // ------Bonus------
-// let newArray = [...[,,]];
-// console.log(newArray);
-
+let newArray = [...[,,]];
+console.log(newArray);
+// ------Bonus------ My prediction : [undefined, undefined]
+console.log('--------');
 
 // 🌟 Exercise 4 : Employees
 // Instructions
 // Using this array:
-
-// const users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
-//              { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
-//              { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
-//              { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
-//              { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
-//              { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
-//              { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
+console.log('Exercise 4 : Employees')
+const users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+             { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+             { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+             { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+             { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+             { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
+             { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
 // Using the map() method, push into a new array the firstname of the user and a welcome message. You should get an array that looks like this :
 // const welcomeStudents = ["Hello Bradley", "Hello Chloe", "Hello Jonathan", "Hello Michael", "Hello Robert", "Hello Wes", "Hello Zach"]
+
+const welcomeStudents = users.map(user => `Hello ${user.firstName}`);
+console.log(welcomeStudents);
+
 
 
 // 2. Using the filter() method, create a new array, containing only the Full Stack Residents.
 
+const fullStackResident = users.filter((user) => {
+    return user.role === "Full Stack Resident"
+})
+console.log(fullStackResident)
 
 // 3. Bonus : Chain the filter method with a map method, to return an array containing only the lastName of the Full Stack Residents.
 
+const fullStackResidentLastNames = users
+    .filter(user => user.role === "Full Stack Resident")
+    .map(resident => resident.lastName);
 
+console.log(fullStackResidentLastNames);
+console.log('--------');
 
 // 🌟 Exercise 5 : Star Wars
+console.log('Exercise 5 : Star Wars')
 // Instructions
-// Using this array const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
+// Using this array 
+
+const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
 
 // Use the reduce() method to combine all of these into a single string.
 
+const epicPhrase = epic.reduce((accumulator, currentValue) => {
+    return accumulator + ' ' + currentValue;
+});
+
+console.log(epicPhrase);
+console.log('--------');
 
 // 🌟 Exercise 6 : Employees #2
+console.log('Exercise 6 : Employees #2')
 // Instructions
 // Using this object:
 
-// const students = [{name: "Ray", course: "Computer Science", isPassed: true}, 
-//                {name: "Liam", course: "Computer Science", isPassed: false}, 
-//                {name: "Jenner", course: "Information Technology", isPassed: true}, 
-//                {name: "Marco", course: "Robotics", isPassed: true}, 
-//                {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
-//                {name: "Jamie", course: "Big Data", isPassed: false}];
+const students = [{name: "Ray", course: "Computer Science", isPassed: true}, 
+                  {name: "Liam", course: "Computer Science", isPassed: false}, 
+                  {name: "Jenner", course: "Information Technology", isPassed: true}, 
+                  {name: "Marco", course: "Robotics", isPassed: true}, 
+                  {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
+                  {name: "Jamie", course: "Big Data", isPassed: false}
+                ];
+
 // Using the filter() method, create a new array, containing the students that passed the course.
 // Bonus : Chain the filter method with a forEach method, to congratulate the students with their name and course name (ie. “Good job Jenner, you passed the course in Information Technology”, “Good Job Marco you passed the course in Robotics” ect…)
+
+const studentsPassed = students
+    .filter(student => student.isPassed === true)
+    .forEach(student => console.log(`Good job ${student.name}, you passed the course in ${student.course}`))
+console.log('--------');
--- Week6/Day4/Practise/script.js
+++ Week6/Day4/Practise/script.js
@@ -1,147 +1,147 @@
-//foreach
-console.log('\nMy Version\n')
-const numbers = [10,11,12,15,20];
+// //foreach
+// console.log('\nMy Version\n')
+// const numbers = [10,11,12,15,20];
 
-numbers.forEach((element) => {
-  if (element % 2 === 0)
-  console.log(element)
-})
-console.log('\nNew Version\n')
-numbers.forEach((element) => {
-  element % 2 === 0 && console.log(element)
-})
-
-
-const myArr = [1, 2, 3, 4, 5, 6, 7, 8];
-myArr.some((element) => {
-    element % 2 === 0
-}); // false because we do not return anything
-
-//1st example
-const myArr = [1, 2, 3, 4, 5, 6, 7, 8];
-myArr.some((value)=> { return (value%2 == 0); });
-//output : true 
-//because there is at least one even number in the array
-
-//2nd example
-myArr.some((value)=> { return (value < 0); });
-//output: false 
-//because there is no negative number in the array
-
-//If the arrow function is only returning a value you don't need the return keyword
-//If the arrow function takes only one parameter you can omit the parentheses
-myArr.some(value => value < 0 );
-//output: false 
-//because there is no negative number in the array
-
-//3rd example
-function isEven(x) {
-  console.log(x) 
-  return (x % 2 == 0);
-}
-
-myArr.some((value) => isEven(value));
-//output : 1 2 true
-//stops iterating as soon as an even number is found 
+// numbers.forEach((element) => {
+//   if (element % 2 === 0)
+//   console.log(element)
+// })
+// console.log('\nNew Version\n')
+// numbers.forEach((element) => {
+//   element % 2 === 0 && console.log(element)
+// })
 
 
+// const myArr = [1, 2, 3, 4, 5, 6, 7, 8];
+// myArr.some((element) => {
+//     element % 2 === 0
+// }); // false because we do not return anything
 
-// EXERCISE
-// My Version
-const words = ["wow", "hey", "bye"];
-let returnedSome = words.some((element) => {
-    console.log(element)
-    return element[0] === "h"
-})
+// //1st example
+// const myArr = [1, 2, 3, 4, 5, 6, 7, 8];
+// myArr.some((value)=> { return (value%2 == 0); });
+// //output : true 
+// //because there is at least one even number in the array
 
-console.log(returnedSome)
+// //2nd example
+// myArr.some((value)=> { return (value < 0); });
+// //output: false 
+// //because there is no negative number in the array
 
-let returnedStartsWith = words.some((element) => {
-    return element.startsWith("h");
-});
+// //If the arrow function is only returning a value you don't need the return keyword
+// //If the arrow function takes only one parameter you can omit the parentheses
+// myArr.some(value => value < 0 );
+// //output: false 
+// //because there is no negative number in the array
 
-console.log(returnedStartsWith)
+// //3rd example
+// function isEven(x) {
+//   console.log(x) 
+//   return (x % 2 == 0);
+// }
 
-const myArr = [1, 2, 3, 4, 5, 6, 7, 8];
-
-let returnedEvery = myArr.every((element) => {
-    console.log(element)
-    return element < 9
-})
-
-console.log(returnedEvery)
-
-
-const words = ["hello", "hey", "hola"];
-wordsEvery = words.every((element)=> {
-    console.log(element)
-    return element[0] === "h"
-})
-
-console.log(wordsEvery)
-
-Map
-
-const originalArray = [1, 4, 9, 16];
-
-const newArr = originalArray.map((element) => {
-    if (element > 4) {
-      return element * 2;
-    } else {
-      return element;
-    }
-});
-
-console.log(newArr)
-
-const myArr = [10, 20, 30, 40];
-const newArr = myArr.map((element, i) => {
-    return {
-        value: element,
-        index: i,
-    }
-})
-
-console.log(newArr)
-
-Map returns a new array with manipulated elements
+// myArr.some((value) => isEven(value));
+// //output : 1 2 true
+// //stops iterating as soon as an even number is found 
 
 
-Filter
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// // EXERCISE
+// // My Version
+// const words = ["wow", "hey", "bye"];
+// let returnedSome = words.some((element) => {
+//     console.log(element)
+//     return element[0] === "h"
+// })
 
-const newArr = words.filter((element) => {
-    return element.length > 6;
-})
+// console.log(returnedSome)
 
-console.log(newArr)
+// let returnedStartsWith = words.some((element) => {
+//     return element.startsWith("h");
+// });
 
-let party = [
-    {
-      desert: 'Chocolate Mousse',
-      calories: 30,
-    },
-    {
-      desert: 'Apple Pie',
-      calories: 15,
-    },
-    {
-      desert: 'Croissant',
-      calories: 50,
-    },
-    {
-      desert: 'Strawberry Icecream',
-      calories: 5,
-    },
-  ]
+// console.log(returnedStartsWith)
 
-  const totalCalories = party.reduce((acc, dessert) => {
-    if (dessert.desert !== 'Apple Pie') {
-        return acc + dessert.calories;  // Returns immediately if this is true
-    }
-    return acc;  // Acts like an 'else' because it returns only if the 'if' condition was false
-}, 0);
+// const myArr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// let returnedEvery = myArr.every((element) => {
+//     console.log(element)
+//     return element < 9
+// })
+
+// console.log(returnedEvery)
 
 
-console.log(totalCalories);
+// const words = ["hello", "hey", "hola"];
+// wordsEvery = words.every((element)=> {
+//     console.log(element)
+//     return element[0] === "h"
+// })
+
+// console.log(wordsEvery)
+
+// Map
+
+// const originalArray = [1, 4, 9, 16];
+
+// const newArr = originalArray.map((element) => {
+//     if (element > 4) {
+//       return element * 2;
+//     } else {
+//       return element;
+//     }
+// });
+
+// console.log(newArr)
+
+// const myArr = [10, 20, 30, 40];
+// const newArr = myArr.map((element, i) => {
+//     return {
+//         value: element,
+//         index: i,
+//     }
+// })
+
+// console.log(newArr)
+
+// Map returns a new array with manipulated elements
+
+
+// Filter
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const newArr = words.filter((element) => {
+//     return element.length > 6;
+// })
+
+// console.log(newArr)
+
+// let party = [
+//     {
+//       desert: 'Chocolate Mousse',
+//       calories: 30,
+//     },
+//     {
+//       desert: 'Apple Pie',
+//       calories: 15,
+//     },
+//     {
+//       desert: 'Croissant',
+//       calories: 50,
+//     },
+//     {
+//       desert: 'Strawberry Icecream',
+//       calories: 5,
+//     },
+//   ]
+
+//   const totalCalories = party.reduce((acc, dessert) => {
+//     if (dessert.desert !== 'Apple Pie') {
+//         return acc + dessert.calories;  // Returns immediately if this is true
+//     }
+//     return acc;  // Acts like an 'else' because it returns only if the 'if' condition was false
+// }, 0);
+
+
+// console.log(totalCalories);
