7b1941e9c4be51ad1abba81278f302ef8403e98c
EgorChet <egor.chetverikov.1997@gmail.com>
2023-12-28 20:17:03 +0200

    Update - 2023-12-28 20:17:03

---
+++ Week6/Day4/Daily_Challenge/CarInventory.js
@@ -0,0 +1,53 @@
+// Daily Challenge: Car Inventory
+// Last Updated: September 22nd, 2023
+
+// What You Will Learn :
+// Use advanced array methods
+// Instructions
+// Part I
+console.log("Part 1 The function should loop through the array of object and return the first car with the name “Honda”")
+
+// Create a function getCarHonda(carInventory) that takes a single parameter. carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+// The function should
+// loop through the array of object and return the first car with the name “Honda”.
+// then, return a string in the format This is a {car_make} {car_model} from {car_year}.
+// Hint : Find an array method that returns the value of the first element in an array that pass a test.
+// Use the cars inventory below:
+const inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+
+const getCarHonda = (carInventory) => {
+    const car = carInventory.find(car => car.car_make === "Honda");
+    return car ? `This is a ${car.car_make} ${car.car_model} from ${car.car_year}.` : 'No Honda cars found.';
+};
+
+
+console.log(getCarHonda(inventory));
+console.log('--------');
+
+// Part II
+console.log('Part 2 The function should return an inventory that is sorted by car_year, ascending')
+// Create a function sortCarInventoryByYear(carInventory) that takes a single parameter. carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+// the function should return an inventory that is sorted by car_year, ascending.
+// Hint : Check out this tutorial on the sort method
+// Use the cars inventory below:
+const sortCarInventoryByYear = (carInventory) => {
+    return carInventory.sort((a, b) => a.car_year - b.car_year);
+};
+console.log(sortCarInventoryByYear(inventory));
+console.log('--------');
+
+// The output should be
+
+// [
+//   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+// ];
---
+++ Week6/Day4/Daily_Challenge/GoWildcats.js
@@ -0,0 +1,60 @@
+// Daily Challenge : Go Wildcats
+console.log('Daily Challenge : Go Wildcats')
+// Last Updated: September 22nd, 2023
+
+// What You Will Learn :
+// Use the advanced array methods
+
+
+// Instructions
+// Using this array:
+
+const gameInfo = [
+ {
+   username: "john",
+   team: "red",
+   score: 5,
+   items: ["ball", "book", "pen"]
+ },
+ {
+   username: "becky",
+   team: "blue",
+   score: 10,
+   items: ["tape", "backpack", "pen"]
+ },
+ {
+   username: "susy",
+   team: "red",
+   score: 55,
+   items: ["ball", "eraser", "pen"]
+ },
+ {
+   username: "tyson",
+   team: "green",
+   score: 1,
+   items: ["book", "pen"]
+ },
+];
+// Create an array using forEach that contains all the usernames from the gameInfo array, add an exclamation point (ie. “!”) to the end of every username.
+// The new array should look like this :
+// const usernames = ["john!", "becky!", "susy!", "tyson!"]
+console.log('1. Create an array using forEach that contains all the usernames from the gameInfo array, add an exclamation point')
+const usernames = []
+gameInfo.forEach(gamer => usernames.push(`${gamer.username}!`));
+console.log(usernames)
+console.log('--------');
+
+// 2. Create an array using forEach that contains the usernames of all players with a score bigger than 5.
+// The new array should look like this :
+// const winners = ["becky", "susy"]
+console.log('2. Create an array using forEach that contains the usernames of all players with a score bigger than 5')
+const fiveAndMore = []
+gameInfo.forEach(gamer => gamer.score >5 && fiveAndMore.push(gamer.username))
+console.log(fiveAndMore)
+console.log('--------');
+
+// 3. Find and display the total score of the users. (Hint: The total score is 71)
+console.log('3. Find and display the total score of the users')
+const totalScore = gameInfo.reduce((acc, gamer) => acc + gamer.score, 0)
+console.log(totalScore)
+console.log('--------');
